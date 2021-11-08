bca029f3cd252bbd281de926b8f233b424d65331
yoyojacky <yoyojacky@users.noreply.github.com>
2021-11-08 12:41:04 +0800

    add try-catch part
    ignore runtimeError message.

--- demo_codes/Project_23_Rotary_encoder.py
+++ demo_codes/Project_23_Rotary_encoder.py
@@ -1,6 +1,8 @@
 from rotary import Rotary
 import utime as time
 
+
+# parameter order is Pin0, Pin1, Pin2 ---> dt, clk, sw 
 rotary = Rotary(0, 1, 2)
 
 val = 0
@@ -17,9 +19,12 @@ def rotary_changed(change):
         print('PRESSED')
     elif change == Rotary.SW_RELEASE:
         print('RELEASED')
-        
-rotary.add_handler(rotary_changed)
 
+try:
+    rotary.add_handler(rotary_changed)
+except RuntimeError:
+    pass
 
 while True:
     time.sleep(0.1)
+    
